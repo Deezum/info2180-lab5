5896e07e3e9ca0079d7360a1828984d800721d18
Deezum <beadleselena@gmail.com>
2021-11-06 15:58:57 -0500

    Edited

--- world.js
+++ world.js
@@ -17,6 +17,25 @@ window.onload = function(){
        
 
     });
+
+    var citybtnlookup= document.getElementById("cities");
+    citybtnlookup.addEventListener("click", function(){
+        let country = document.getElementById("country").value;
+        let url = "http://localhost/info2180-lab5/world.php?context=";
+        
+        let dhttp = new XMLHttpRequest();
+        let result = document.getElementById("result");
+        dhttp.onreadystatechange = function(){
+            if(this.readyState == XMLHttpRequest.DONE && this.status ==200){
+                result.innerHTML = dhttp.response;
+            }
+        };
+
+        dhttp.open("GET", url+country,true );
+        dhttp.send();
+       
+
+    });
  
 
 
