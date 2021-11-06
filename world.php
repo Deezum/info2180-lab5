05949499bf477a14b4ebf207c7bb29bfb3ec6cd3
Deezum <beadleselena@gmail.com>
2021-11-06 12:20:38 -0500

    world.php

--- world.php
+++ world.php
@@ -1,14 +1,18 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$country =  filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+$countryquery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$results = $countryquery->fetchAll(PDO::FETCH_ASSOC);
+?>
+<?php
+    header('Access-Control-Allow-Origin: *');
 ?>
 <ul>
 <?php foreach ($results as $row): ?>
