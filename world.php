a0d9753dd865a10557d5e14593c59110a3b68607
Deezum <beadleselena@gmail.com>
2021-11-06 15:59:26 -0500

    Edited

--- world.php
+++ world.php
@@ -11,6 +11,8 @@ $country =  filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
 $countryquery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $countryquery->fetchAll(PDO::FETCH_ASSOC);
 $context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
+$cityquery = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code=countries.code WHERE countries.name LIKE '%$context%'");
+$city = $cityquery->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
@@ -18,7 +20,7 @@ $context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
     header('Access-Control-Allow-Origin: *');
 ?>
 
-
+<?php if(isset($country)&&(!isset($context))) :?>
 <table>
           <thead>
             <tr>
@@ -38,7 +40,24 @@ $context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
               </tr>
             <?php endforeach; ?>
           </tbody>
-        </table>
+</table>
+<?php elseif (isset($context)): ?>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>District</th> 
+    <th>Population</th>
+  </tr>
+  <?php foreach ($city as $row): ?>
+    <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['district'] ?></td>
+      <td><?= $row['population'] ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
+<?php endif ?>
+
 
 
 
